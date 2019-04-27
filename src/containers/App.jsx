3aec34f1749491ec31b28a715557bd01d6837429
Thomas <trasmussen@redventures.com>
2019-04-26 23:54:01 -0400

    removed context, adding redux

---
+++ src/containers/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListContainer from './ListContainer';
+
+const App = () => (
+  <ListContainer />
+);
+
+export default App;
--- src/contexts/ListContext.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const ListContext = React.createContext();
-export default ListContext;
